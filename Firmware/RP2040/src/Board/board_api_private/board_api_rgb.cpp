2f245858c513e0aa73ff95fad50b59629cd03056
chaosweilder <timehero.final.anti.form@gmail.com>
2026-02-26 00:27:59 +0100

    Update board_api_rgb.cpp

--- Firmware/RP2040/src/Board/board_api_private/board_api_rgb.cpp
+++ Firmware/RP2040/src/Board/board_api_private/board_api_rgb.cpp
@@ -10,7 +10,7 @@ namespace board_api_rgb {
 
 WS2812& get_ws2812(WS2812::DataFormat format) {
 #if defined(RGB_PXL_PIN)
-    static WS2812 ws2812_grb = WS2812(RGB_PXL_PIN, 1, pio1, 0, WS2812::FORMAT_GRB);
+    static WS2812 ws2812_grb = WS2812(RGB_PXL_PIN, 1, pio1, 1, WS2812::FORMAT_GRB);
 #endif
 #if defined(GRB_PXL_PIN)
     static WS2812 ws2812_rgb = WS2812(GRB_PXL_PIN, 1, pio1, 1, WS2812::FORMAT_RGB);
