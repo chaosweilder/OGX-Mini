660323f68412d2c00a48027207cb918292424259
chaosweilder <timehero.final.anti.form@gmail.com>
2026-02-25 23:34:47 +0100

    Update board_api_rgb.cpp

--- Firmware/RP2040/src/Board/board_api_private/board_api_rgb.cpp
+++ Firmware/RP2040/src/Board/board_api_private/board_api_rgb.cpp
@@ -8,9 +8,11 @@
 
 namespace board_api_rgb {
 
-WS2812& get_ws2812() {
-    static WS2812 ws2812 = WS2812(RGB_PXL_PIN, 1, pio1, 0, WS2812::FORMAT_GRB);
-    return ws2812;
+WS2812& get_ws2812(WS2812::DataFormat format, uint pin) {
+    static WS2812 ws2812_grb = WS2812(RGB_PXL_PIN, 1, pio1, 0, WS2812::FORMAT_GRB);
+    static WS2812 ws2812_rgb = WS2812(GRB_PXL_PIN, 1, pio1, 1, WS2812::FORMAT_RGB);
+    // Nota: sm diferente (0 y 1) para no colisionar en el mismo PIO
+    return (format == WS2812::FORMAT_RGB) ? ws2812_rgb : ws2812_grb;
 }
 
 void init() {
@@ -24,8 +26,13 @@ void init() {
 }
 
 void set_led(uint8_t r, uint8_t g, uint8_t b) {
-    get_ws2812().setPixelColor(0, WS2812::RGB(r, g, b));
-    get_ws2812().show();
+#if defined(GRB_PXL_PIN)
+    get_ws2812(WS2812::FORMAT_RGB, GRB_PXL_PIN).setPixelColor(0, WS2812::RGB(r, g, b));
+    get_ws2812(WS2812::FORMAT_RGB, GRB_PXL_PIN).show();
+#else
+    get_ws2812(WS2812::FORMAT_GRB, RGB_PXL_PIN).setPixelColor(0, WS2812::RGB(r, g, b));
+    get_ws2812(WS2812::FORMAT_GRB, RGB_PXL_PIN).show();
+#endif
 }
 
 } // namespace board_api_rgb
