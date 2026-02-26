9c735144c86464cfdd5796db863f624d30d7e833
chaosweilder <timehero.final.anti.form@gmail.com>
2026-02-26 01:17:33 +0100

    Update board_api_rgb.cpp

--- Firmware/RP2040/src/Board/board_api_private/board_api_rgb.cpp
+++ Firmware/RP2040/src/Board/board_api_private/board_api_rgb.cpp
@@ -10,7 +10,7 @@ namespace board_api_rgb {
 
 WS2812& get_ws2812(WS2812::DataFormat format) {
 #if defined(GRB_PXL_PIN)
-    static WS2812 ws2812_grb = WS2812(GRB_PXL_PIN, 1, pio1, 0, WS2812::FORMAT_GRB);
+    static WS2812 ws2812_grb = WS2812(GRB_PXL_PIN, 1, pio1, 1, WS2812::FORMAT_GRB);
 #endif
 #if defined(RGB_PXL_PIN)
     static WS2812 ws2812_rgb = WS2812(RGB_PXL_PIN, 1, pio1, 0, WS2812::FORMAT_RGB);
@@ -39,7 +39,7 @@ void set_led(uint8_t r, uint8_t g, uint8_t b) {
     get_ws2812(WS2812::FORMAT_RGB).setPixelColor(0, WS2812::RGB(r, g, b));
     get_ws2812(WS2812::FORMAT_RGB).show();
 #elif defined(GRB_PXL_PIN)
-    get_ws2812(WS2812::FORMAT_GRB).setPixelColor(0, WS2812::GRB(g, r, b));
+    get_ws2812(WS2812::FORMAT_GRB).setPixelColor(0, WS2812::RGB(g, r, b));
     get_ws2812(WS2812::FORMAT_GRB).show();
 #endif
 }
